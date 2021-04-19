c6c2b9d3e9c583e0c81b6c5bd4be7b0e09e9939e
yt2639 <46978474+yt2639@users.noreply.github.com>
2021-04-20 01:02:51 +0800

    Update checkin.py
    改start里面那个time

--- checkin.py
+++ checkin.py
@@ -27,12 +27,13 @@ def start():
    # print(res)
 
     if 'message' in checkin.text:
-        mess = checkin.json()['message']
-        time = state.json()['data']['leftDays']
-        time = time.split('.')[0]
+#         mess = checkin.json()['message']
+#         time = state.json()['data']['leftDays']
+#         time = time.split('.')[0]
         #print(time)
         if sever == '2':
-            requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+#             requests.get('https://sc.ftqq.com/' + sckey + '.send?text='+mess+'，you have '+time+' days left')
+            requests.get('https://sc.ftqq.com/' + sckey + '.send?text=成功')
     else:
         requests.get('https://sc.ftqq.com/' + sckey + '.send?text=cookie过期')
 
